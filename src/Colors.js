cd682f4ebf0287d90f61900c562fb7f48c0c9b13
GustafJ <gustaf89@hotmail.se>
2020-12-10 15:56:13 +0100

    add colors

---
+++ src/Colors.js
@@ -0,0 +1,11 @@
+const Colors = {
+	mediumSeaGreen: '#1FB163',
+	flame: '#DC542E',
+	earieBlack: '#222222',
+	floralWhite: '#FEFDF4',
+	paleCerulean: '#9FC3DD',
+	greenBlueCrayola: '#3F88C5',
+	carrotOrange: '#ED9B40',
+};
+
+export default Colors;
