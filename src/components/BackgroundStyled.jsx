fcf9031b3543ba9268c10c584941716d6d0a6f13
“erikaahlstrom” <info@erikasoldh.se>
2020-12-14 11:52:23 +0100

    Add baclground image

---
+++ src/components/BackgroundStyled.jsx
@@ -0,0 +1,12 @@
+import colors from '../Colors'
+import styled from 'styled-components';
+import img from '../img/bg.png'
+const BackgroundStyled = styled.div`
+    background-image: url(${img});
+    width: 100%;
+    height: 85vh;
+    background-size: cover;
+    background-position: center;
+`
+
+export {BackgroundStyled}
