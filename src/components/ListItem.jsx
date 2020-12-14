db8ab16cbd6a6068d2511d621ecf678b96dc741d
GustafJ <gustaf89@hotmail.se>
2020-12-14 09:49:16 +0100

    update gitignre

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -6,7 +6,6 @@ export default function ListItem({ children, listItemData, stock, markets }) {
 
 	return (
 		<>
-		
 			{stock && (
 				<Link to={`stock/${market}/${id}`}>
 					<li>{children}</li>
