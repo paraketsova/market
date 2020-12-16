aa011ca4daf97084aae0fc33452997d38d13d346
“erikaahlstrom” <info@erikasoldh.se>
2020-12-16 11:31:03 +0100

    Add functionality to ButtonBackStyled

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,28 +1,71 @@
-import React from 'react'
-import ListHeaderStyled from './ListHeaderStyled'
-import ChangeSpan from './ChangeSpan'
-import ContainerStyled from './ContainerStyled'
-import DetailsItemStyled from './DetailsItemStyled'
-import PriceSpanStyled from './PriceSpanStyled'
+import React from "react";
+import ListHeaderStyled from "./ListHeaderStyled";
+import ChangeSpan from "./ChangeSpan";
+import ContainerStyled from "./ContainerStyled";
+import DetailsItemStyled from "./DetailsItemStyled";
+import PriceSpanStyled from "./PriceSpanStyled";
+import { Link } from "react-router-dom";
+import ButtonBackStyled from "./ButtonBackStyled";
 
-export default function Details({itemData}) {    
-    return (
-        <ContainerStyled>  
-            <div> 
-                <ListHeaderStyled>
-                    <h3>{itemData.name}</h3>            
-                </ListHeaderStyled>
-                    
-                <DetailsItemStyled><span>Market:</span> <PriceSpanStyled>{itemData.market.toUpperCase()}</PriceSpanStyled></DetailsItemStyled>
-                <DetailsItemStyled><span>Source:</span> <PriceSpanStyled>{itemData.source}</PriceSpanStyled></DetailsItemStyled>
-                <DetailsItemStyled><span>Price:</span> <PriceSpanStyled>{itemData.price}</PriceSpanStyled></DetailsItemStyled>
-                <DetailsItemStyled><span>Today:</span> <ChangeSpan number={itemData.today}/></DetailsItemStyled>   
-                {!isNaN(itemData.w1) && <DetailsItemStyled><span>One week:</span> <ChangeSpan number={itemData.w1}/></DetailsItemStyled>}
-                {!isNaN(itemData.y3) && <DetailsItemStyled><span>3 years:</span> <ChangeSpan number={itemData.y3}/></DetailsItemStyled>}
-                {!isNaN(itemData.y5) && <DetailsItemStyled><span>5 years:</span> <ChangeSpan number={itemData.y5}/></DetailsItemStyled>}
-                {!isNaN(itemData.mtd) && <DetailsItemStyled><span>Month to date:</span> <ChangeSpan number={itemData.mtd}/></DetailsItemStyled>}
-                {!isNaN(itemData.ytd) && <DetailsItemStyled><span>Year to date:</span> <ChangeSpan number={itemData.ytd}/></DetailsItemStyled>}
-            </div>        
-        </ContainerStyled>
-    )
+// Prints all details for each item on detail page.
+export default function Details({ itemData, type }) {
+  return (
+    <ContainerStyled>
+      <div>
+        <ListHeaderStyled>
+          <h3>{itemData.name}</h3>
+          <Link to={`/${type}`}>
+            <ButtonBackStyled>Back</ButtonBackStyled>
+          </Link>
+        </ListHeaderStyled>
+
+        <DetailsItemStyled>
+          <span>Market:</span>{" "}
+          <PriceSpanStyled>{itemData.market.toUpperCase()}</PriceSpanStyled>
+        </DetailsItemStyled>
+
+        <DetailsItemStyled>
+          <span>Source:</span>{" "}
+          <PriceSpanStyled>{itemData.source}</PriceSpanStyled>
+        </DetailsItemStyled>
+
+        <DetailsItemStyled>
+          <span>Price:</span>{" "}
+          <PriceSpanStyled>{itemData.price}</PriceSpanStyled>
+        </DetailsItemStyled>
+
+        <DetailsItemStyled>
+          <span>Today:</span> <ChangeSpan number={itemData.today} />
+        </DetailsItemStyled>
+        
+        {/* Uses !isNaN to correct for 0:s from the API 
+            only prints detail item if we get it from the API. */}
+        {!isNaN(itemData.w1) && (
+          <DetailsItemStyled>
+            <span>One week:</span> <ChangeSpan number={itemData.w1} />
+          </DetailsItemStyled>
+        )}
+        {!isNaN(itemData.y3) && (
+          <DetailsItemStyled>
+            <span>3 years:</span> <ChangeSpan number={itemData.y3} />
+          </DetailsItemStyled>
+        )}
+        {!isNaN(itemData.y5) && (
+          <DetailsItemStyled>
+            <span>5 years:</span> <ChangeSpan number={itemData.y5} />
+          </DetailsItemStyled>
+        )}
+        {!isNaN(itemData.mtd) && (
+          <DetailsItemStyled>
+            <span>Month to date:</span> <ChangeSpan number={itemData.mtd} />
+          </DetailsItemStyled>
+        )}
+        {!isNaN(itemData.ytd) && (
+          <DetailsItemStyled>
+            <span>Year to date:</span> <ChangeSpan number={itemData.ytd} />
+          </DetailsItemStyled>
+        )}
+      </div>
+    </ContainerStyled>
+  );
 }
