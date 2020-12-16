71664b97f21d15c55e6491b1bf6fe1cb29c8588a
“erikaahlstrom” <info@erikasoldh.se>
2020-12-16 11:31:45 +0100

    Add functionality to ButtonBackStyled

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import { LinkListStyled } from './LinkListStyled'
-import ListContainerStyled from './ListContainerStyled';
-import ListHeaderStyled from './ListHeaderStyled'
-import ListItem from './ListItem';
+import React from "react";
+import ButtonBackStyled from "./ButtonBackStyled";
+import { LinkListStyled } from "./LinkListStyled";
+import ListContainerStyled from "./ListContainerStyled";
+import ListHeaderStyled from "./ListHeaderStyled";
+import ListItem from "./ListItem";
+import { Link } from "react-router-dom";
 
+// Prints all the correct data to a list for all categories.
 export default function List(props) {
-  const {list, title, args, markets} = props;
+  const { list, title, args, markets } = props;
   return (
+    // Print correct header
     <ListContainerStyled>
-
-
-    <LinkListStyled>
-      <ListHeaderStyled><h3>{title}</h3></ListHeaderStyled>
-      {list.map((listItem) => (
-        <ListItem key={listItem[0]} listItemData={listItem} type={args}>
-          {markets ? listItem[0] : listItem[1].name}
-        </ListItem>
-      ))}
-    </LinkListStyled>
-
+      <LinkListStyled>
+        <ListHeaderStyled>
+          {title === "Stocks" ? (
+            <>
+              <h3>{list[0][1].market}</h3>
+              <Link to="/markets">
+                <ButtonBackStyled>Back</ButtonBackStyled>
+              </Link>
+            </>
+          ) : (
+            <>
+              <h3>{title}</h3>
+              <Link to="/">
+                <ButtonBackStyled>Back</ButtonBackStyled>
+              </Link>
+            </>
+          )}
+        {/* Print correct data*/}        
+        </ListHeaderStyled>
+        {list.map((listItem) => (
+          <ListItem key={listItem[0]} listItemData={listItem} type={args}>
+            {markets ? listItem[0] : listItem[1].name}
+          </ListItem>
+        ))}
+      </LinkListStyled>
     </ListContainerStyled>
-  )
+  );
 }
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import Details from '../components/Details'
 
+// Prints data to all detail pages on the site.
 export default function DetailPage(props) {
     const [detailData, setDetailData] = useState(null);
+    const [type, setType] = useState(null);
     
     function dataFetch(){
         let url;
@@ -10,15 +12,20 @@ export default function DetailPage(props) {
         const market = props.match.params.market;
         const stock = props.match.params.stock;
         const pathname = props.history.location.pathname;
-
+        
+        // Fetch different url depending on which page is clicked.
         if (pathname.includes('crypto')){
             url = `https://market-data-collector.firebaseio.com/market-collector/crypto/usd/${id}.json`
+            setType('crypto')
         } else if (pathname.includes('currencies')){
             url = `https://market-data-collector.firebaseio.com/market-collector/currencies/sek/${id}.json`
+            setType('currencies')
         } else if (pathname.includes('indexes')){
             url = `https://market-data-collector.firebaseio.com/market-collector/indexes/se/${id}.json`
+            setType('indexes')
         } else if (pathname.includes('stock')){
             url = `https://market-data-collector.firebaseio.com/market-collector/markets/${market}/${stock}.json`
+            setType(`market/${market}`)
         }
         
         fetch(url)
@@ -29,7 +36,7 @@ export default function DetailPage(props) {
     useEffect(dataFetch,[]);
     return (
         <div>
-            {detailData && <Details itemData={detailData}/>}
+            {detailData && <Details type={type} itemData={detailData}/>}
         </div>
     )
 }
